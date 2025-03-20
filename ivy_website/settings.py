9f4adf4e1185b8380e68167ce1f1bac78eb06d11
Dylan Rice <2769579r@student.gla.ac.uk>
2025-03-20 21:50:02 +0000

    Add videos and work and education

--- ivy_website/settings.py
+++ ivy_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '3gc^o(c4svsl%emdo194-73lk7e4t79vt8l379!75y9@3e%w(3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ivyfawcett.pythonanywhere.com', ]
+ALLOWED_HOSTS = ['ivyfawcett.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
